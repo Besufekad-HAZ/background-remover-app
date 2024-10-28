f21ec9ad6f40cc4a4936c4e8400b4e4fdfbdd3a8
Besufekad-HAZ <besurye@gmail.com>
2024-10-28 20:41:13 +0300

    Style the layout for the results page

--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -1,5 +1,12 @@
 const Result = () => {
-  return <div>Result</div>;
+  return (
+    <div className="mx-4 my-3 lg:mx-44 mt-14 min-h-[75vh]">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente deserunt
+      ea voluptate aliquam, repellat accusamus libero iure ad voluptas nihil
+      molestias pariatur labore delectus cum placeat temporibus commodi soluta.
+      Numquam quia repudiandae maxime aspernatur!
+    </div>
+  );
 };
 
 export default Result;
