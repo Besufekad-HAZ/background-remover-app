035880f372286256592a4c1bab2edc1a4b6a42a4
Besufekad-HAZ <besurye@gmail.com>
2024-10-28 20:43:19 +0300

    Add right and left section for the original and converted images

--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -1,10 +1,29 @@
+import { assets } from "../assets/assets";
+
+assets
 const Result = () => {
   return (
-    <div className="mx-4 my-3 lg:mx-44 mt-14 min-h-[75vh]">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente deserunt
-      ea voluptate aliquam, repellat accusamus libero iure ad voluptas nihil
-      molestias pariatur labore delectus cum placeat temporibus commodi soluta.
-      Numquam quia repudiandae maxime aspernatur!
+    <div className="mx-4 my-3 mt-14 min-h-[75vh] lg:mx-44">
+      <div>
+        {/* Image Container */}
+        <div>
+          {/* Left Side (Original Image)*/}
+          <div>
+            <h2 className="mb-4 mt-4 bg-gradient-to-r from-gray-900 to-gray-400 bg-clip-text pb-2 text-center text-2xl font-semibold text-transparent sm:mb-20 md:text-3xl lg:text-4xl">
+              Original Image
+            </h2>
+            <img width={500} src={assets.image_w_bg} alt="Original Image" />
+          </div>
+
+          {/* Right Side (Result Image)*/}
+          <div>
+            <h2 className="mb-4 mt-4 bg-gradient-to-r from-gray-900 to-gray-400 bg-clip-text pb-2 text-center text-2xl font-semibold text-transparent sm:mb-20 md:text-3xl lg:text-4xl">
+              Result Image
+            </h2>
+            <img width={500} src={assets.image_wo_bg} alt="Result Image" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
