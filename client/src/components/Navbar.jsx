2963fc27ff0f3559db880567c3f2b587c98fdcc0
Besufekad-HAZ <besurye@gmail.com>
2024-10-28 15:30:29 +0300

    Update the navbar styling

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,12 +4,12 @@ const Navbar = () => {
   return (
     <div className="mx-4 flex items-center justify-between py-3 lg:mx-44">
       <img className="w-32 sm:w-44" src={assets.logo} alt="Navbar Logo" />
-      <button className="flex justify-between bg-zinc-800 text-white">
+      <button className="flex items-center gap-4 rounded-full bg-zinc-800 px-4 py-2 text-sm text-white sm:px-8 sm:py-3">
         Get Started
         <img
           src={assets.arrow_icon}
           alt="Get started Arrow Icon"
-          className="bg-yellow-400"
+          className="w-3 sm:w-4"
         />
       </button>
     </div>
