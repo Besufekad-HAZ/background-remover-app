eaae0f54627024f8f7f2cccf49ffe2e20488a75c
Besufekad-HAZ <besurye@gmail.com>
2024-10-28 20:55:34 +0300

    Arrange the flex layout of the results page

--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -4,22 +4,19 @@ assets;
 const Result = () => {
   return (
     <div className="mx-4 my-3 mt-14 min-h-[75vh] lg:mx-44">
-      <div>
+      <div className="rounded-lg bg-white px-8 py-6 drop-shadow-sm">
         {/* Image Container */}
-        <div className="flex rounded-lg bg-white px-8 py-6 drop-shadow-sm">
+        <div className="flex grid-cols-2 flex-col gap-8 sm:grid">
           {/* Left Side (Original Image)*/}
           <div>
-            <p className="mb-4 mt-4 bg-gradient-to-r from-gray-900 to-gray-400 bg-clip-text pb-2 text-center text-2xl font-semibold text-transparent sm:mb-20 md:text-3xl lg:text-4xl">
-              Original Image
-            </p>
+            {/* Title */}
+            <p className="">Original Image</p>
             <img width={500} src={assets.image_w_bg} alt="Original Image" />
           </div>
 
           {/* Right Side (Result Image)*/}
           <div>
-            <p className="mb-4 mt-4 bg-gradient-to-r from-gray-900 to-gray-400 bg-clip-text pb-2 text-center text-2xl font-semibold text-transparent sm:mb-20 md:text-3xl lg:text-4xl">
-              Background Removed
-            </p>
+            <p className="">Background Removed</p>
             <img
               width={500}
               src={assets.image_wo_bg}
