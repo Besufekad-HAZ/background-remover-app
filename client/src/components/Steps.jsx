5993fbfc390d42a35ed180cbc234ae8550d775e8
Besufekad-HAZ <besurye@gmail.com>
2024-10-28 16:55:04 +0300

    Fill in the the steps section

--- client/src/components/Steps.jsx
+++ client/src/components/Steps.jsx
@@ -1,5 +1,34 @@
 const Steps = () => {
-  return <div>Steps</div>;
+  return (
+    <div>
+      <h2>
+        Steps to remove background <br /> image in seconds
+      </h2>
+      <div className="flex">
+        <div>
+          <img src="" alt="" />
+          <div>
+            <p>Upload image</p>
+            <p>Select an image file to upload.</p>
+          </div>
+        </div>
+        <div>
+          <img src="" alt="" />
+          <div>
+            <p>Remove Background</p>
+            <p>Automatically remove the background from the image.</p>
+          </div>
+        </div>
+        <div>
+          <img src="" alt="" />
+          <div>
+            <p>Download image</p>
+            <p>Save the edited image to your device.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Steps;
