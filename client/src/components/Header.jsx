e891d4fe66307c3b188447b9db50ac080a5494a5
Besufekad-HAZ <besurye@gmail.com>
2024-10-28 19:35:00 +0300

    Update the button and header h1 font gradient

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
       <div>
         <h1 className="text-4xl font-bold leading-tight text-neutral-700 xl:text-5xl 2xl:text-6xl">
           Remove the <br className="max-md:hidden" />
-          <span className="bg-gradient-to-r from-orange-700 to-orange-400 bg-clip-text text-transparent">
+          <span className="bg-gradient-to-r from-purple-700 to-pink-500 bg-clip-text text-transparent">
             background
           </span>{" "}
           from <br className="max-md:hidden" />
@@ -20,7 +20,7 @@ const Header = () => {
         <div>
           <input type="file" name="" id="upload1" hidden />
           <label
-            className="m-auto inline-flex cursor-pointer gap-3 rounded-full bg-gradient-to-r from-orange-700 to-orange-400 px-8 py-3.5 transition-all duration-700 hover:scale-105"
+            className="m-auto inline-flex cursor-pointer gap-3 rounded-full bg-gradient-to-r from-purple-700 to-pink-500 px-8 py-3.5 transition-all duration-700 hover:scale-105"
             htmlFor="upload1"
           >
             <img width={20} src={assets.upload_btn_icon} alt="" />
