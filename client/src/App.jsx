53ff67de3dfa05841ca43ef9d0fc01bdbf8db322
Besufekad-HAZ <besurye@gmail.com>
2024-10-28 15:05:17 +0300

    Start working on the navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Result from "./pages/Result";
+import BuyCredit from "./pages/BuyCredit";
+import Navbar from "./components/Navbar";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/result" element={<Result />} />
+        <Route path="/buy" element={<BuyCredit />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
