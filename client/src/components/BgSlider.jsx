c1250acf5057a3f43ba8bdbdee24a0ceedd91fdc
Besufekad-HAZ <besurye@gmail.com>
2024-10-28 18:27:41 +0300

    Add custom css for the bg slider icons and position

--- client/src/components/BgSlider.jsx
+++ client/src/components/BgSlider.jsx
@@ -11,7 +11,7 @@ const BgSlider = () => {
   return (
     <div>
       {/* Title*/}
-      <h2 className="mt-4 bg-gradient-to-r from-gray-900 to-gray-400 bg-clip-text pb-2 text-center text-2xl font-semibold text-transparent md:text-3xl lg:text-4xl">
+      <h2 className="mb-12 mt-4 bg-gradient-to-r from-gray-900 to-gray-400 bg-clip-text pb-2 text-center text-2xl font-semibold text-transparent sm:mb-20 md:text-3xl lg:text-4xl">
         Remove Background With High <br /> Quality and Accuracy
       </h2>
 
@@ -24,14 +24,14 @@ const BgSlider = () => {
         />
         {/* Foreground Image */}
         <img
-          className="absolute left-0 top-0 h-full w-full"
+          className="absolute left-0 top-[-1px] h-full w-full"
           src={assets.image_wo_bg}
           alt="image without background for slider"
           style={{ clipPath: `inset(0 0 0 ${sliderPosition}%)` }}
         />
         {/* Slider */}
         <input
-          className="aboslute slider left-1/2 top-1/2 z-10 -translate-x-1/2 -translate-y-1/2 transform"
+          className="slider absolute left-1/2 top-1/2 z-10 w-full -translate-x-1/2 -translate-y-1/2 transform"
           type="range"
           min="0"
           max="100"
