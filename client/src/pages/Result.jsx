159c02d14b3af1d350601754eed7a4605ea2ed2a
Besufekad-HAZ <besurye@gmail.com>
2024-10-28 20:48:03 +0300

    Give border and background for the left side and right side image

--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -6,7 +6,7 @@ const Result = () => {
     <div className="mx-4 my-3 mt-14 min-h-[75vh] lg:mx-44">
       <div>
         {/* Image Container */}
-        <div className="flex">
+        <div className="flex rounded-lg bg-white px-8 py-6 drop-shadow-sm">
           {/* Left Side (Original Image)*/}
           <div>
             <p className="mb-4 mt-4 bg-gradient-to-r from-gray-900 to-gray-400 bg-clip-text pb-2 text-center text-2xl font-semibold text-transparent sm:mb-20 md:text-3xl lg:text-4xl">
@@ -20,7 +20,11 @@ const Result = () => {
             <p className="mb-4 mt-4 bg-gradient-to-r from-gray-900 to-gray-400 bg-clip-text pb-2 text-center text-2xl font-semibold text-transparent sm:mb-20 md:text-3xl lg:text-4xl">
               Background Removed
             </p>
-            <img width={500} src={assets.image_wo_bg} alt="Result Image" />
+            <img
+              width={500}
+              src={assets.image_wo_bg}
+              alt="Background Removed Image"
+            />
           </div>
         </div>
       </div>
