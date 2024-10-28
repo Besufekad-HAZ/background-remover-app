17d356ae220c1dff93a597e20333e7f88ad0b2b4
Besufekad-HAZ <besurye@gmail.com>
2024-10-28 20:14:34 +0300

    Add image and paragraph in the footer section

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,5 +1,14 @@
+import { assets } from "../assets/assets";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <img src={assets.logo} alt="Logo icon for the footer" />
+      <p className="text-neutral-500">
+        © 2024 Remove Background. All rights reserved.
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
