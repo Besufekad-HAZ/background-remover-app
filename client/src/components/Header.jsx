613006d3718f6e14cb2bdaaedfc6eaf63bf9906b
Besufekad-HAZ <besurye@gmail.com>
2024-10-28 15:47:06 +0300

    Create a layout for the left side if the header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,30 @@
+import { assets } from "../assets/assets";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      {/* Left side  */}
+      <div>
+        <h1>
+          Remove the <br /> <span>background</span> from <br />
+          your images for free.
+        </h1>
+        <p>
+          Upload your image and get a clean, <br /> transparent background in
+          seconds.
+        </p>
+        <div>
+          <input type="file" name="" id="upload1" hidden />
+          <label htmlFor="upload1">
+            <img src={assets.upload_btn_icon} alt="" />
+            <p>Upload your image</p>
+          </label>
+        </div>
+      </div>
+      {/* Right side  */}
+      <div></div>
+    </div>
+  );
 };
 
 export default Header;
