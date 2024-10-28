4f8e0d8869997dac7e2973c497854e346f5c6869
Besufekad-HAZ <besurye@gmail.com>
2024-10-28 16:36:47 +0300

    Make the Header text responsive

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,25 +6,25 @@ const Header = () => {
       {/* Left side  */}
       <div>
         <h1 className="text-4xl font-bold leading-tight text-neutral-700 xl:text-5xl 2xl:text-6xl">
-          Remove the <br />
+          Remove the <br className="max-md:hidden" />
           <span className="bg-gradient-to-r from-orange-700 to-orange-400 bg-clip-text text-transparent">
             background
           </span>{" "}
-          from <br />
+          from <br className="max-md:hidden" />
           your images for free.
         </h1>
         <p className="my-6 text-[15px] text-gray-500">
-          Upload your image and get a clean, <br /> transparent background in
-          seconds.
+          Upload your image and get a clean, <br className="max-sm:hidden" />{" "}
+          transparent background in seconds.
         </p>
         <div>
           <input type="file" name="" id="upload1" hidden />
           <label
-            className="m-auto inline-flex cursor-pointer gap-3 bg-gradient-to-r from-orange-700 to-orange-400 px-8 py-3.5 transition-all duration-700 hover:scale-105"
+            className="m-auto inline-flex cursor-pointer gap-3 rounded-full bg-gradient-to-r from-orange-700 to-orange-400 px-8 py-3.5 transition-all duration-700 hover:scale-105"
             htmlFor="upload1"
           >
-            <img src={assets.upload_btn_icon} alt="" />
-            <p>Upload your image</p>
+            <img width={20} src={assets.upload_btn_icon} alt="" />
+            <p className="text-sm text-white">Upload your image</p>
           </label>
         </div>
       </div>
