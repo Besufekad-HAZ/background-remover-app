f687002c01012fbb0440b308c3d12673b3b61dde
Besufekad-HAZ <besurye@gmail.com>
2024-10-28 20:24:10 +0300

    Make the social icons in the footer responsive

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <p className="border-1 flex-1 border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden">
         © 2024 Remove Background. All rights reserved.
       </p>
-      <div>
+      <div className="flex gap-1">
         <img width={40} src={assets.facebook_icon} alt="facebook icon" />
         <img width={40} src={assets.twitter_icon} alt="twitter icon" />
         <img width={40} src={assets.google_plus_icon} alt="googl plus icon" />
