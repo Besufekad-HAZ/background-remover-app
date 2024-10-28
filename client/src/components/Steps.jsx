b5978dfea95c5baa3bc2447d84dc9d2f0c63a9f1
Besufekad-HAZ <besurye@gmail.com>
2024-10-28 17:06:58 +0300

    Add gradient color to the Large text in the steps section

--- client/src/components/Steps.jsx
+++ client/src/components/Steps.jsx
@@ -1,26 +1,28 @@
+import { assets } from "../assets/assets";
+
 const Steps = () => {
   return (
-    <div>
-      <h2>
+    <div className="mx-4 py-20 lg:mx-44 xl:py-40">
+      <h2 className="mt-4 bg-gradient-to-r from-gray-900 to-gray-400 bg-clip-text text-center text-2xl font-semibold text-transparent md:text-3xl lg:text-4xl">
         Steps to remove background <br /> image in seconds
       </h2>
       <div className="flex">
         <div>
-          <img src="" alt="" />
+          <img src={assets.upload_icon} alt="" />
           <div>
             <p>Upload image</p>
             <p>Select an image file to upload.</p>
           </div>
         </div>
         <div>
-          <img src="" alt="" />
+          <img src={assets.remove_bg_icon} alt="" />
           <div>
             <p>Remove Background</p>
             <p>Automatically remove the background from the image.</p>
           </div>
         </div>
         <div>
-          <img src="" alt="" />
+          <img src={assets.download_icon} alt="" />
           <div>
             <p>Download image</p>
             <p>Save the edited image to your device.</p>
