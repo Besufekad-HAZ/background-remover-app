4c56b1bef39f656946631143c0a4277b05a7792f
Besufekad-HAZ <besurye@gmail.com>
2024-10-28 19:09:52 +0300

    Use Grid to layout testimonials section

--- client/src/components/Testimonials.jsx
+++ client/src/components/Testimonials.jsx
@@ -1,5 +1,30 @@
+import { testimonialsData } from "../assets/assets";
+
 const Testimonials = () => {
-  return <div>Testimonials</div>;
+  return (
+    <div>
+      {/* Title */}
+      <h2 className="bg-gradient-to-r from-gray-900 to-gray-500 bg-clip-text py-5 pb-2 text-center text-2xl font-semibold text-transparent md:text-3xl lg:text-4xl">
+        Customer Testimonials
+      </h2>
+      <div className="mx-auto grid max-w-4xl grid-cols-1 gap-10 px-4 py-8 md:grid-cols-2">
+        {/* Testimonial */}
+        {testimonialsData.map((item, index) => (
+          <div key={index}>
+            <p>”</p>
+            <p>{item.text}</p>
+            <div>
+              <img src={item.image} alt="testimonial avatar image" />
+              <div>
+                <p>{item.author}</p>
+                <p>{item.jobTitle}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Testimonials;
