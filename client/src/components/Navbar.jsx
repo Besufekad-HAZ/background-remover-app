07e1b885032aea36c66662cb8d6e293ce66a9869
Besufekad-HAZ <besurye@gmail.com>
2024-10-30 14:40:40 +0300

    Implement the clerk login in the navbar Component

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,19 @@
+import { useClerk } from "@clerk/clerk-react";
 import { assets } from "../assets/assets";
 import { Link } from "react-router-dom";
 const Navbar = () => {
+  const { openSignIn } = useClerk();
   return (
     <div className="mx-4 flex items-center justify-between py-3 lg:mx-44">
       <Link to="/">
         <img className="w-32 sm:w-44" src={assets.logo} alt="Navbar Logo" />
       </Link>
-      <button className="flex items-center gap-4 rounded-full bg-zinc-800 px-4 py-2 text-sm text-white sm:px-8 sm:py-3">
+      <button
+        onClick={() => {
+          openSignIn({});
+        }}
+        className="flex items-center gap-4 rounded-full bg-zinc-800 px-4 py-2 text-sm text-white sm:px-8 sm:py-3"
+      >
         Get Started
         <img
           src={assets.arrow_icon}
