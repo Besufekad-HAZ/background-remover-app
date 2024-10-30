8b670fb608c0fd15755cea0a00381e00842cd4fb
Besufekad-HAZ <besurye@gmail.com>
2024-10-30 15:25:08 +0300

    Initialize check and start ther server

--- server/server.js
+++ server/server.js
@@ -7,3 +7,17 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import Razorpay from 'razorpay';
+
+
+// App config
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+// API routes
+app.get('/', (req, res) => {
+    res.status(200).send('API is running');
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
