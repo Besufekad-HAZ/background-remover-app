d44411d04a04e28fb007d99e5acea35eb227d8f0
Besufekad-HAZ <besurye@gmail.com>
2024-10-30 12:39:46 +0300

    Make the plans div flex

--- client/src/pages/BuyCredit.jsx
+++ client/src/pages/BuyCredit.jsx
@@ -9,7 +9,7 @@ const BuyCredit = () => {
       <h2 className="mt-2 bg-gradient-to-r from-gray-900 to-gray-500 bg-clip-text py-5 pb-2 text-center text-2xl font-semibold text-transparent sm:mb-20 md:text-3xl lg:text-4xl">
         Choose the plan that&apos;s right for you
       </h2>
-      <div>
+      <div className="flex justify-center gap-5">
         {/* Plans */}
         {plans.map((item, index) => (
           <div key={index}>
