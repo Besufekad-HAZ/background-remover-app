d3b268f9f8e8465309e9e1300efe9d583a84e73e
Besufekad-HAZ <besurye@gmail.com>
2024-10-30 12:20:57 +0300

    Create heading text and button for the buy credit page

--- client/src/pages/BuyCredit.jsx
+++ client/src/pages/BuyCredit.jsx
@@ -1,5 +1,12 @@
 const BuyCredit = () => {
-  return <div className="mb-10 min-h-[80vh] pt-14 text-center"></div>;
+  return (
+    <div className="mb-10 min-h-[80vh] pt-14 text-center">
+      <button>Our Plans</button>
+      <h2 className="mt-2 bg-gradient-to-r from-gray-900 to-gray-500 bg-clip-text py-5 pb-2 text-center text-2xl font-semibold text-transparent sm:mb-20 md:text-3xl lg:text-4xl">
+        Choose the plan that’s right for you
+      </h2>
+    </div>
+  );
 };
 
 export default BuyCredit;
