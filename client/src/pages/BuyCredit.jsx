47baabde3d00fc808d9433415f30afaa9de8de39
Besufekad-HAZ <besurye@gmail.com>
2024-10-30 12:36:19 +0300

    Added return implicitly by using parentheses () around the JSX. and Added a key prop to the outer div to ensure each element has a unique identifier, which is required when rendering lists in React.

--- client/src/pages/BuyCredit.jsx
+++ client/src/pages/BuyCredit.jsx
@@ -1,3 +1,5 @@
+import { assets, plans } from "../assets/assets";
+
 const BuyCredit = () => {
   return (
     <div className="mb-10 min-h-[80vh] pt-14 text-center">
@@ -5,6 +7,19 @@ const BuyCredit = () => {
       <h2 className="mt-2 bg-gradient-to-r from-gray-900 to-gray-500 bg-clip-text py-5 pb-2 text-center text-2xl font-semibold text-transparent sm:mb-20 md:text-3xl lg:text-4xl">
         Choose the plan that&apos;s right for you
       </h2>
+      <div>
+        {/* Plans */}
+        {plans.map((item, index) => (
+          <div key={index}>
+            <img width={40} src={assets.logo_icon} alt="Logo icon" />
+            <p>{item.id}</p>
+            <p>{item.desc}</p>
+            <p>
+              <span>${item.price}</span>/ {item.credits} credits
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
