5024ceb79eb286f24bca646a58c093535cb0b118
Besufekad-HAZ <besurye@gmail.com>
2024-10-30 14:53:12 +0300

    Implement the clerk login and user management in the navbar Component

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,26 +1,33 @@
-import { useClerk } from "@clerk/clerk-react";
+import { useClerk, UserButton, UserProfile, useUser } from "@clerk/clerk-react";
 import { assets } from "../assets/assets";
 import { Link } from "react-router-dom";
 const Navbar = () => {
   const { openSignIn } = useClerk();
+  const { isSignedIn, user } = useUser();
   return (
     <div className="mx-4 flex items-center justify-between py-3 lg:mx-44">
       <Link to="/">
         <img className="w-32 sm:w-44" src={assets.logo} alt="Navbar Logo" />
       </Link>
-      <button
-        onClick={() => {
-          openSignIn({});
-        }}
-        className="flex items-center gap-4 rounded-full bg-zinc-800 px-4 py-2 text-sm text-white sm:px-8 sm:py-3"
-      >
-        Get Started
-        <img
-          src={assets.arrow_icon}
-          alt="Get started Arrow Icon"
-          className="w-3 sm:w-4"
-        />
-      </button>
+      {isSignedIn ? (
+        <div>
+          <UserButton />
+        </div>
+      ) : (
+        <button
+          onClick={() => {
+            openSignIn({});
+          }}
+          className="flex items-center gap-4 rounded-full bg-zinc-800 px-4 py-2 text-sm text-white sm:px-8 sm:py-3"
+        >
+          Get Started
+          <img
+            src={assets.arrow_icon}
+            alt="Get started Arrow Icon"
+            className="w-3 sm:w-4"
+          />
+        </button>
+      )}
     </div>
   );
 };
