139289f7981128786adde6b984fdb2a62f7262bd
Besufekad-HAZ <besurye@gmail.com>
2024-10-30 12:38:35 +0300

    Style our plans button

--- client/src/pages/BuyCredit.jsx
+++ client/src/pages/BuyCredit.jsx
@@ -3,7 +3,9 @@ import { assets, plans } from "../assets/assets";
 const BuyCredit = () => {
   return (
     <div className="mb-10 min-h-[80vh] pt-14 text-center">
-      <button>Our Plans</button>
+      <button className="rounded-full border border-gray-400 px-10 py-2">
+        Our Plans
+      </button>
       <h2 className="mt-2 bg-gradient-to-r from-gray-900 to-gray-500 bg-clip-text py-5 pb-2 text-center text-2xl font-semibold text-transparent sm:mb-20 md:text-3xl lg:text-4xl">
         Choose the plan that&apos;s right for you
       </h2>
