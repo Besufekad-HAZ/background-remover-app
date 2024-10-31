ff26ea10a11af9123fa21e6d423aca0c28624043
Besufekad-HAZ <besurye@gmail.com>
2024-11-01 00:45:49 +0300

    Add a GET route for fetching user credits with authentication middleware

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,8 +1,11 @@
 import express from "express";
 import { clerkWebhooks } from "../controllers/userControllers.js";
+import { AuthenticationApiRequestFactory } from "svix/dist/openapi/apis/AuthenticationApi.js";
+import authUser from "../middlewares/auth.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/webhooks", clerkWebhooks);
+userRouter.get("/credits", authUser, userCredits);
 
 export default userRouter;
