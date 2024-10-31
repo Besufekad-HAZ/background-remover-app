f422a150c5edbe26969ccf4d949d38fd469d2fc5
Besufekad-HAZ <besurye@gmail.com>
2024-10-31 23:02:10 +0300

    Add clerkWebhooks function to handle Clerk webhooks verification using svix library

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,5 +1,19 @@
+import { Webhook } from "svix";
+
 // API Controller Function to Manage Clerk User with database
 // http://localhost:5000/api/user/webhooks
 const clerkWebhooks = async (req, res) => {
-    
+    try {
+        // Create a Svix instance with clerk webhook secret
+        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
+
+        await whook.verify(JSON.stringify(req.body), {
+            "svix-id": req.headers["svix-id"],
+            "svix-timestamp": req.headers["svix-timestamp"],
+            "svix-signature": req.headers["svix-signature"],
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: 'Internal Server Error' });
+    }
 }
