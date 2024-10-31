9f500ec95b42d611bd8a79e82eb4b4b0a8503d20
Besufekad-HAZ <besurye@gmail.com>
2024-11-01 01:02:10 +0300

    Set up AppContext and AppContextProvider with initial state and loadCreditsData function

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,14 +1,19 @@
 import { createContext, useState } from "react";
 import PropTypes from "prop-types";
+import { useAuth } from "@clerk/clerk-react";
 
 const AppContext = createContext();
 
 const AppContextProvider = (props) => {
   const [creditor, setCreditor] = useState(false);
 
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+  const {getToken} = useAuth();
+
   const loadCreditsData = async () => {
     try {
-        
+
     }
   const value = {
     // Add your context values here
