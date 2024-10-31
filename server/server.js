18824ab86337743a98fdfc126ce30a8eab097c5e
Besufekad-HAZ <besurye@gmail.com>
2024-10-31 23:33:38 +0300

    Import the userRoutes inside the server.js and use it + remove node_modules unnecessary folder

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import Razorpay from 'razorpay';
 import connectDB from './configs/mongodb.js';
+import userRouter from './routes/userRoutes.js';
 
 
 // App config
@@ -23,6 +24,8 @@ app.use(cors());
 app.get('/', (req, res) => {
     res.status(200).send('API is running');
 })
+
+app.use('/api/user', userRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
