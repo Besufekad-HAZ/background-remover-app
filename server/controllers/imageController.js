cbaf916f79c651018b8e476ac25e20a86db4c2c7
Besufekad-HAZ <besurye@gmail.com>
2024-11-04 22:11:44 +0300

    Create multer.js file to recive formdata and pictures from the front ned

--- server/controllers/imageController.js
+++ server/controllers/imageController.js
@@ -6,20 +6,7 @@ import userModel from "../models/User.js";
 // Controller function to remove background from image
 const removeBackground = async (req, res) => {
     try {
-        const { image } = req.body;
-        const formData = new FormData();
-        formData.append("image", fs.createReadStream(image));
-        formData.append("size", "auto");
-        formData.append("format", "png");
-        formData.append("crop", "true");
-        formData.append("type", "auto");
-        formData.append("format", "png");
-        formData.append("crop", "true");
-        formData.append("type", "auto");
-        formData.append("format", "png");
-        formData.append("crop", "true");
-        formData.append("type", "auto");
-        formData.append("format", "png");
+        
     } catch (error) {
         console.log(error.message);
         res.json({ success: false, message: error.message });
