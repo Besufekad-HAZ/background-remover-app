4a3e8c818cae281d5195aad5e4e50b6db4592613
Besufekad-HAZ <besurye@gmail.com>
2024-11-04 22:21:30 +0300

    Import authUser inside the imageRoutes file

--- server/routes/imageRoutes.js
+++ server/routes/imageRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { removeBackground } from "../controllers/imageController.js";
 import { upload } from "../middlewares/multer.js";
+import authUser from "../middlewares/authUser.js";
 
 const router = express.Router();
