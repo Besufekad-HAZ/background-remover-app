e9d2d7aa50ab587c30f4b3bebe3f649ba5882dd2
Besufekad-HAZ <besurye@gmail.com>
2024-11-04 22:41:24 +0300

    Read the uploaded image file from the specified path

--- server/controllers/imageController.js
+++ server/controllers/imageController.js
@@ -18,6 +18,11 @@ const removeBackground = async (req, res) => {
       return res.json({ success: false, message: "Insufficient credits", credits: user.creditBalance });
     }
 
+    const imagePath = req.file.path;
+
+    // Reading the image file
+    const imageFile = fs.createReadStream(imagePath);
+
   } catch (error) {
     console.log(error.message);
     res.json({ success: false, message: error.message });
