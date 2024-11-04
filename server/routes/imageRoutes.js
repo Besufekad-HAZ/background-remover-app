bbb321f83c12c6087ec9fbfdebac8d9d3c32adc4
Besufekad-HAZ <besurye@gmail.com>
2024-11-04 22:22:41 +0300

    Create a new route for test inside the imageROutes

--- server/routes/imageRoutes.js
+++ server/routes/imageRoutes.js
@@ -3,4 +3,15 @@ import { removeBackground } from "../controllers/imageController.js";
 import { upload } from "../middlewares/multer.js";
 import authUser from "../middlewares/authUser.js";
 
+const imageRouter = express.Router();
+
+imageRouter.post(
+  "/remove-bg",
+  authUser,
+  upload.single("image"),
+  removeBackground
+);
+
+export default imageRouter;
+
 const router = express.Router();
