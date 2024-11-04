833fd392c1f4db1348b4df84e9f42ef2c12bde04
Besufekad-HAZ <besurye@gmail.com>
2024-11-04 22:15:25 +0300

    Remove the destionation section from the multer cofig file

--- server/middlewares/multer.js
+++ server/middlewares/multer.js
@@ -2,9 +2,6 @@ import multer from "multer";
 
 // Multer configuration
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads");
-  },
   filename: function (req, file, cb) {
     cb(null, `${Date.now()}-${file.originalname}`);
   },
