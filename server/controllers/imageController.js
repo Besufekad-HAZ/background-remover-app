3c4a2c863fbc8ec8f8597c0aa25db965a16a981b
Besufekad-HAZ <besurye@gmail.com>
2024-11-04 22:30:13 +0300

    Fetch user by clerkId using findById method

--- server/controllers/imageController.js
+++ server/controllers/imageController.js
@@ -6,6 +6,9 @@ import userModel from "../models/User.js";
 // Controller function to remove background from image
 const removeBackground = async (req, res) => {
   try {
+    const { clerkId } = req.body;
+
+    const user = await userModel.findById(clerkId);
   } catch (error) {
     console.log(error.message);
     res.json({ success: false, message: error.message });
