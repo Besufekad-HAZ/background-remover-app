13e935f4175663035ea9b82b5b17290c24bde200
Besufekad-HAZ <besurye@gmail.com>
2024-11-05 15:15:00 +0300

    Create payment routes (routes/paymentRoutes.js):

---
+++ server/routes/paymentRoutes.js
@@ -0,0 +1,13 @@
+import express from "express";
+import {
+  initializePayment,
+  verifyPayment,
+} from "../controllers/paymentController.js";
+import authUser from "../middlewares/auth.js";
+
+const paymentRouter = express.Router();
+
+paymentRouter.post("/initialize", authUser, initializePayment);
+paymentRouter.get("/verify", verifyPayment);
+
+export default paymentRouter;
